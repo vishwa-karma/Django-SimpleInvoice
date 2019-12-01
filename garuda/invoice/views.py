381b179db3ea863a74b290632e89a3baf9e84009
Ravi <ravi92teja@gmail.com>
2019-12-01 17:19:39 +0530

    Added seva kitchen app and it's url and view

--- garuda/invoice/views.py
+++ garuda/invoice/views.py
@@ -114,6 +114,6 @@ def dummy(request, pk):
 
     return render(request, 'invoice/invoice_detail3.html',  {'data':queryset})
 
-def seva():
-    render ('seva/home.html')
+def seva(request):
+    return render (request, 'seva/home.html')
 
