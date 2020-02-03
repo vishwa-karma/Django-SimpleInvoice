d19a4679536f97d401b85fe851ee8845293755ce
Ravi <ravi92teja@gmail.com>
2020-02-03 09:37:04 +0530

    Trying signup Again

--- garuda/garuda/urls.py
+++ garuda/garuda/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
