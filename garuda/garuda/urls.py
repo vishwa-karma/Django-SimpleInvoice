e91695eebb03373b94617a9ce2661f9db0451a17
Ravi <ravi92teja@gmail.com>
2020-02-03 09:39:43 +0530

    Restructured templates

--- garuda/garuda/urls.py
+++ garuda/garuda/urls.py
@@ -18,12 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("django.contrib.auth.urls")),
-    path('signup/', views.SignUp.as_view(), name='signup'),
     path('invoice/', include("invoice.urls")),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     #path('mf/', include("invoice.urls")),
