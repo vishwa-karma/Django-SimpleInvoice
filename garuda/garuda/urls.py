9fdf7daff4751d92f96a3fe714e44bc2f44c970c
Ravi <ravi92teja@gmail.com>
2020-02-03 09:01:50 +0530

    Authentication initiated

--- garuda/garuda/urls.py
+++ garuda/garuda/urls.py
@@ -20,8 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("invoice.urls")),
+    path('', include("django.contrib.auth.urls")),
     path('invoice/', include("invoice.urls")),
-    path('mf/', include("invoice.urls")),
-    path('seva/', include("invoice.urls")),
+    #path('mf/', include("invoice.urls")),
+    #path('seva/', include("invoice.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
